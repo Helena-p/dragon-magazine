896c23a57141ad5d8212dbedf39a859fc8a2ee78
Helena <helenasleterkesto@gmail.com>
2021-06-22 13:42:03 +0200

    Iterating over array of objects

--- src/script.js
+++ src/script.js
@@ -2,26 +2,40 @@
 
 const app = document.querySelector("#app");
 const endpoint = "https://vanillajsacademy.com/api/dragons.json";
-let html = app.innerHTML;
 
 async function getArticles() {
     try {
+        // Fetch data from API
         let response = await fetch(endpoint);
-
+        // Check if response return with no errors,
+        // otherwise throw status of response to errorhandler
         if (!response.ok) throw response.status;
 
+        // Convert response to json data
+        // Check if response contain data if not throw error
         let data = await response.json();
         if (!data) throw "No data";
-        console.log(data.publication);
-        console.log(data.articles);
+        //console.log(data.articles[0]);
 
         let header = data.publication;
+        let articles = data.articles;
+        console.log(articles[0].title);
+        console.log(articles[0].author);
+        console.log(articles[0].pubdate);
+        console.log(articles[0].article);
 
-        app.innerHTML = `<h1 class="header">${header}</h1> ${data.articles
-            .map(function (article) {
-                return `${article}`;
-            })
-            .join("")}`;
+        app.innerHTML = `
+        <h1 class="header">${header}</h1> 
+        <div class="article-container">
+            <ul>
+            ${articles
+                .map(function (article) {
+                    return `<li>${article}</li>`;
+                })
+                .join("")}
+            </ul>
+            
+        </div>`;
     } catch (error) {
         app.innerHTML =
             "There have been an error. Please refresh page or try again later.";
